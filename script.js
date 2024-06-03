20126f5db7e17a51ccb028db6f17b458001b67ff
Zx Loh <59832510+Zx-Loh@users.noreply.github.com>
2024-06-03 15:17:54 +0800

    Add hover function
    Added javascript event listener to each box that changes its background color permanently to black after it is mouseovered

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const mainContainer = document.querySelector(".vertical-container")
+const gridDefault = 16
 
 
 
-for (let i=0; i<16; i++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("horizontal-container");
+function createGrid(x) {
+    for (let i=0; i<x; i++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("horizontal-container");
 
-    for (let i=0; i<16; i++) {
-        const columnDiv = document.createElement("div");
-        columnDiv.classList.add("box")
-        rowDiv.appendChild(columnDiv);
+        for (let i=0; i<x; i++) {
+            const columnDiv = document.createElement("div");
+            columnDiv.classList.add("box");
+
+            columnDiv.addEventListener("mouseover", () => {
+                columnDiv.style.cssText = "background: black;";
+            });
+
+
+            rowDiv.appendChild(columnDiv);
+        }
+
+        mainContainer.appendChild(rowDiv)
     }
-
-    mainContainer.appendChild(rowDiv)
 }
+
+
+createGrid(gridDefault)
